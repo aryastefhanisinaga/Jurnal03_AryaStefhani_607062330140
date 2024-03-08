9586b27633cadb35d46b5d61c27bd8b3e900308d
aryastefhanisinaga <145308425+aryastefhanisinaga@users.noreply.github.com>
2024-03-08 09:29:27 +0700

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,15 @@
+public class Book {
+    String title;
+    String author;
+    int year;
+
+    public Book(String title, String author, int year) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+
+    public void display() {
+        System.out.println("Title: " + title + ", Author: " + author + ", Year: " + year);
+    }
+}
